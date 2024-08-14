7e44311a65c6acc1489ee58f1e68e840612cc35d
Chikof <chiko@envs.net>
2024-08-14 14:12:55 +0100

    feat: file upload

--- index.js
+++ index.js
@@ -3,18 +3,31 @@ var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+const upload = require('multer')();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.json({ error: 'No file uploaded' });
+  }
 
+  const { originalname, mimetype, size } = req.file;
 
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
